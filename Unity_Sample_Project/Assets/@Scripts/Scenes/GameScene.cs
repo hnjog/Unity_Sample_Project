108e72998c29230ca4dc15fab62e582973c46043
Hyun <hnjogo@gmail.com>
2024-09-25 18:58:52 +0900

    임시 테스트용 코드

--- Unity_Sample_Project/Assets/@Scripts/Scenes/GameScene.cs
+++ Unity_Sample_Project/Assets/@Scripts/Scenes/GameScene.cs
@@ -14,19 +14,24 @@ public class GameScene : BaseScene
         SceneType = Define.EScene.GameScene;
 
         // Map 로드
-        GameObject map = Managers.Resource.Instantiate("BaseMap");
-        map.transform.position = Vector3.zero;
-        map.name = "@BaseMap";
+        Managers.Map.LoadMap("BaseMap");
 
-        HeroCamp camp = Managers.Object.Spawn<HeroCamp>(new Vector3(-10, -5, 0), 0);
+        HeroCamp camp = Managers.Object.Spawn<HeroCamp>(Vector3.zero, 0);
+        camp.SetCellPos(new Vector3Int(0, 0, 0), true);
 
-        for (int i = 0; i <5; i++)
+        for (int i = 0; i <10; i++)
         {
             // 게임 신에서 직접 생성하면 관리가 어려워짐
             // 생성용 클래스
             //int heroTemplateID = HERO_WIZARD_ID + Random.Range(0,5);
             int heroTemplateID = HERO_WIZARD_ID;
-            Hero temp = Managers.Object.Spawn<Hero>(new Vector3(-10 + Random.Range(-5,5), -5 + Random.Range(-5, 5), 0), heroTemplateID);
+
+            Vector3Int randCellPos = new Vector3Int(0 + Random.Range(-3, 3), 0 + Random.Range(-3, 3), 0);
+            if (Managers.Map.CanGo(randCellPos) == false)
+                continue;
+
+            Hero hero = Managers.Object.Spawn<Hero>(new Vector3Int(1, 0, 0), heroTemplateID);
+            hero.SetCellPos((Vector3Int)randCellPos,true);
         }
 
         CameraController cameraController = Camera.main.GetOrAddComponent<CameraController>();
@@ -37,7 +42,7 @@ public class GameScene : BaseScene
         {
             //Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0), MONSTER_BEAR_ID);
             //Managers.Object.Spawn<Monster>(new Vector3Int(1, 1, 0), MONSTER_SLIME_ID);
-            Managers.Object.Spawn<Monster>(new Vector3(3, 1, 0), MONSTER_GOBLIN_ARCHER_ID);
+            //Managers.Object.Spawn<Monster>(new Vector3(3, 1, 0), MONSTER_GOBLIN_ARCHER_ID);
         }
 
         {
