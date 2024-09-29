7ff027b1ab2b36a7f5a08c70fdf2740c8bf7431f
Hyun <hnjogo@gmail.com>
2024-09-29 17:54:29 +0900

    테스트용 코드

--- Unity_Sample_Project/Assets/@Scripts/Scenes/GameScene.cs
+++ Unity_Sample_Project/Assets/@Scripts/Scenes/GameScene.cs
@@ -23,15 +23,16 @@ public class GameScene : BaseScene
         {
             // 게임 신에서 직접 생성하면 관리가 어려워짐
             // 생성용 클래스
-            //int heroTemplateID = HERO_WIZARD_ID + Random.Range(0,5);
-            int heroTemplateID = HERO_WIZARD_ID;
+            int heroTemplateID = HERO_WIZARD_ID + Random.Range(0,5);
+            //int heroTemplateID = HERO_KNIGHT_ID;
 
             Vector3Int randCellPos = new Vector3Int(0 + Random.Range(-3, 3), 0 + Random.Range(-3, 3), 0);
             if (Managers.Map.CanGo(randCellPos) == false)
                 continue;
 
             Hero hero = Managers.Object.Spawn<Hero>(new Vector3Int(1, 0, 0), heroTemplateID);
-            hero.SetCellPos((Vector3Int)randCellPos,true);
+            //hero.SetCellPos((Vector3Int)randCellPos,true);
+            Managers.Map.MoveTo(hero, randCellPos, true);
         }
 
         CameraController cameraController = Camera.main.GetOrAddComponent<CameraController>();
@@ -40,13 +41,12 @@ public class GameScene : BaseScene
         Managers.UI.ShowBaseUI<UI_Joystick>();
 
         {
-            //Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0), MONSTER_BEAR_ID);
-            //Managers.Object.Spawn<Monster>(new Vector3Int(1, 1, 0), MONSTER_SLIME_ID);
-            //Managers.Object.Spawn<Monster>(new Vector3(3, 1, 0), MONSTER_GOBLIN_ARCHER_ID);
+            Monster monster = Managers.Object.Spawn<Monster>(new Vector3(1, 1, 0), MONSTER_SLIME_ID);
+            Managers.Map.MoveTo(monster, new Vector3Int(0, 4, 0), true);
         }
 
         {
-            Env env = Managers.Object.Spawn<Env>(new Vector3(0,2,0),ENV_TREE1_ID);
+            Env env = Managers.Object.Spawn<Env>(new Vector3(0, 2, 0), ENV_TREE1_ID);
             env.EnvState = EEnvState.Idle;
         }
 
