e5b397e6de124c5dd37c821a1445321b05f27396
Hyun <hnjogo@gmail.com>
2024-10-05 17:12:35 +0900

    범위 내의 baseObject 탐색용 함수 추가
    - MapManager에 넣어 차후 특정 범위에 알맞게
      최적화 할 예정

--- Unity_Sample_Project/Assets/@Scripts/Managers/Contents/MapManager.cs
+++ Unity_Sample_Project/Assets/@Scripts/Managers/Contents/MapManager.cs
@@ -165,6 +165,40 @@ public class MapManager
     }
 
     #region Helpers
+    // 타입에 맞는 오브젝트 찾아서 리스트로 return
+    // 오브젝트에서도 존재하지만, range를 받아 특정한 칸 단위로 탐색할 생각
+    // (최적화 예정)
+    public List<T> GatherObjects<T>(Vector3 pos, float rangeX, float rangeY) where T : BaseObject
+    {
+        List<T> objects = new List<T>();
+
+        Vector3Int left = World2Cell(pos + new Vector3(-rangeX, 0));
+        Vector3Int right = World2Cell(pos + new Vector3(+rangeX, 0));
+        Vector3Int bottom = World2Cell(pos + new Vector3(0, -rangeY));
+        Vector3Int top = World2Cell(pos + new Vector3(0, +rangeY));
+        int minX = left.x;
+        int maxX = right.x;
+        int minY = bottom.y;
+        int maxY = top.y;
+
+        for (int x = minX; x <= maxX; x++)
+        {
+            for (int y = minY; y <= maxY; y++)
+            {
+                Vector3Int tilePos = new Vector3Int(x, y, 0);
+
+                // 타입에 맞는 리스트 리턴
+                T obj = GetObject(tilePos) as T;
+                if (obj == null)
+                    continue;
+
+                objects.Add(obj);
+            }
+        }
+
+        return objects;
+    }
+
     public BaseObject GetObject(Vector3Int cellPos)
     {
         // 없으면 null
