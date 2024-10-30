90e7dd4904f9676dc9f1c700d8f4d9a4bec60fac
Hyun <hnjogo@gmail.com>
2024-10-30 16:48:59 +0900

    resource 폴더 변화로 인한 에디터 경로 교체 등

--- Unity_Sample_Project/Assets/@Scripts/Editor/MapEditor.cs
+++ Unity_Sample_Project/Assets/@Scripts/Editor/MapEditor.cs
@@ -61,29 +61,38 @@ public class MapEditor : MonoBehaviour
     [MenuItem("Tools/Create Object Tile Asset %#o")]
     public static void CreateObjectTile()
     {
-        // Monster
+        #region Monster
         Dictionary<int, Data.MonsterData> MonsterDic = LoadJson<Data.MonsterDataLoader, int, Data.MonsterData>("MonsterData").MakeDict();
         foreach (var data in MonsterDic.Values)
         {
-            // Unity 에디터에서 사용할 수 있는 데이터 객체 생성
-            // ScriptableObject는 인스펙터 창 등에서 데이터 객체를 만들기 위해 사용
-            // (에셋 저장도 가능)
-            CustomTile customTile = ScriptableObject.CreateInstance<CustomTile>();
-            customTile.Name = data.DescriptionTextID;
-            customTile.DataId = data.DataId;
-            customTile.ObjectType = Define.EObjectType.Monster;
-
-            string name = $"{data.DataId}_{data.DescriptionTextID}";
-            string path = "Assets/@Resources/TileMaps/Tiles/Dev/Monster";
-            path = Path.Combine(path, $"{name}.Asset");
-
-            if (File.Exists(path))
+            if (data.DataId < 202000)
                 continue;
 
+            CustomTile customTile = ScriptableObject.CreateInstance<CustomTile>();
+            customTile.Name = data.DescriptionTextID;
+            string spriteName = data.IconImage;
+            spriteName = spriteName.Replace(".sprite", "");
+
+            Sprite spr = AssetDatabase.LoadAssetAtPath<Sprite>($"Assets/@Resources/Sprites/Monsters/{spriteName}.png");
+            customTile.sprite = spr;
+            customTile.DataId = data.DataId;
+            customTile.ObjectType = Define.EObjectType.Monster;
+            string name = $"{data.DataId}_{data.DescriptionTextID}";
+            string path = "Assets/@Resources/TileMaps/01_asset/dev/Monster";
+            path = Path.Combine(path, $"{name}.Asset");
+
+            if (path == "")
+                continue;
+
+            if (File.Exists(path))
+            {
+                continue;
+            }
             AssetDatabase.CreateAsset(customTile, path);
         }
+        #endregion
 
-        // Env
+        #region Env
         Dictionary<int, Data.EnvData> Env = LoadJson<Data.EnvDataLoader, int, Data.EnvData>("EnvData").MakeDict();
         foreach (var data in Env.Values)
         {
@@ -94,14 +103,19 @@ public class MapEditor : MonoBehaviour
             customTile.ObjectType = Define.EObjectType.Env;
 
             string name = $"{data.DataId}_{data.DescriptionTextID}";
-            string path = "Assets/@Resources/TileMaps/Tiles/Dev/Env";
+            string path = "Assets/@Resources/TileMaps/01_asset/dev/Env";
             path = Path.Combine(path, $"{name}.Asset");
 
-            if (File.Exists(path))
+            if (path == "")
                 continue;
 
+            if (File.Exists(path))
+            {
+                continue;
+            }
             AssetDatabase.CreateAsset(customTile, path);
         }
+        #endregion
     }
 
     private static Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
