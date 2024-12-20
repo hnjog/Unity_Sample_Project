fb963fa08e845e037fc9ebf5367b2990b16f2c38
Hyun <hnjogo@gmail.com>
2024-12-20 17:44:31 +0900

    팝업 ui 및 아이템용 ui 추가

---
+++ Unity_Sample_Project/Assets/@Scripts/UI/Popup/UI_WaypointPopup.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using static UnityEditor.Progress;
+
+public class UI_WaypointPopup : UI_Popup
+{
+    enum GameObjects
+    {
+        WaypointList
+    }
+
+    enum Buttons
+    {
+        CloseButton,
+    }
+
+    List<UI_StageItem> _items = new List<UI_StageItem>();
+
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        BindObjects(typeof(GameObjects));
+        BindButtons(typeof(Buttons));
+        
+        GetButton((int)Buttons.CloseButton).gameObject.BindEvent(OnClickCloseButton);
+
+        Refresh();
+        return true;
+    }
+
+    public void SetInfo()
+    {
+		Refresh();
+    }
+
+    // 인벤토리 등, '갱신'용 함수는 따로 빼두면 쓸일이 많음
+    // UI 부분과 데이터 부분을 분리하여 관리하는 것이 유용
+    void Refresh()
+    {
+        if (_init == false)
+            return;
+
+        _items.Clear();
+
+		GameObject parent = GetObject((int)GameObjects.WaypointList);
+
+        foreach (var stage in Managers.Map.StageTransition.Stages)
+        {
+            UI_StageItem item = Managers.UI.MakeSubItem<UI_StageItem>(parent.transform);
+
+            item.SetInfo(stage, () =>
+            {
+                Managers.UI.ClosePopupUI(this);
+            });
+
+            _items.Add(item);
+		}
+    }
+
+    void OnClickCloseButton(PointerEventData evt)
+    {
+        Managers.UI.ClosePopupUI(this);
+    }
+}
+
